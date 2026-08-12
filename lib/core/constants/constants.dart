f79f96f068cc855538916815a4a85f851d44d9df
Yuri-cmd <yurim011218@gmail.com>
2026-08-12 12:09:58 -0500

    feat: add API constants and increment build version to 1.1.52+55

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  static const String baseUrl = 'http://217.216.54.88/api/';
-  static const String baseStorageUrl = 'http://217.216.54.88/storage/';
+  static const String baseUrl = 'https://hotelyuma.uk/api/';
+  static const String baseStorageUrl = 'https://hotelyuma.uk/storage/';
 
-  static const String productionUrl = 'http://217.216.54.88/api/';
+  static const String productionUrl = 'https://hotelyuma.uk/api/';
   static const String localUrl = 'http://192.168.100.50/truelove-back/public/api/';
 }
