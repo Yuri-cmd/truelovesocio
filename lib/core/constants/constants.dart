797caeaa858fb7a9be8dbec9ac51afc7a95ef126
Yuri-cmd <yurim011218@gmail.com>
2026-08-12 11:28:15 -0500

    feat: increment app version and add API base URL constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  static const String baseUrl = 'https://magusemail.com/truelove-back/public/api/';
-  static const String baseStorageUrl = 'https://magusemail.com/truelove-back/public/storage/';
-  
-  static const String productionUrl = 'https://magusemail.com/truelove-back/public/api/';
+  static const String baseUrl = 'http://217.216.54.88/api/';
+  static const String baseStorageUrl = 'http://217.216.54.88/storage/';
+
+  static const String productionUrl = 'http://217.216.54.88/api/';
   static const String localUrl = 'http://192.168.100.50/truelove-back/public/api/';
 }
